41ebb94f840a8ebca2ee41c54aaa4bc90c9db81d
yungerobot <git@github.com>
2021-01-05 16:56:44 +0800

    升级至class-based view

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -16,17 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from blog.views import post_list,post_detail
+from blog.views import IndexView, CategoryView, TagView, PostDetailView
 from config.views import links
 from typeidea.custom_site import custom_site
 
 
-
 urlpatterns = [
-    path('', post_list, name='index'),
-    path('category/<category_id>/', post_list, name='category-list'),
-    path('tag/<tag_id>/', post_list, name='tag-list'),
-    path('post/<post_id>.html', post_detail, name='post-detail'),
+#    path('', post_list, name='index'),
+#    path('category/<category_id>/', post_list, name='category-list'),
+#    path('tag/<tag_id>/', post_list, name='tag-list'),
+#    path('post/<post_id>.html', post_detail, name='post-detail'),
+    path('', IndexView.as_view(), name='index'),
+    path('category/<category_id>/', CategoryView.as_view(), name='category-list'),
+    path('tag/<tag_id>/', TagView.as_view(), name='tag-list'),
+    path('post/<post_id>.html', PostDetailView.as_view(), name='post-detail'),
     path('links/', links, name='links'),
     path('super_admin/', admin.site.urls, name='super-admin'),
     path('admin/', custom_site.urls, name='admin'),
